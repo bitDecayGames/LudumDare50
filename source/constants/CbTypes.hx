36255f12e559106e9bc9b5fbca45fe931f0dd5ff
Monday <monday.hopscotch@gmail.com>
2022-04-02 22:52:29 -0600

    get ready for adding sfx. detect collisions

--- source/constants/CbTypes.hx
+++ source/constants/CbTypes.hx
@@ -4,25 +4,10 @@ import nape.callbacks.CbType;
 
 // Callback types
 class CbTypes {
-	public static var CB_BOMB:CbType;
-	public static var CB_CARGO:CbType;
-	public static var CB_SHIP_SENSOR_RANGE:CbType;
-	public static var CB_TERRAIN:CbType;
-	public static var CB_TOWABLE:CbType;
-	public static var CB_DESTRUCTIBLE:CbType;
+	public static var CB_ITEM:CbType;
 
 	public static function initTypes() {
-		CB_BOMB = new CbType();
-		trace("CB_BOMB is " + CB_BOMB);
-		CB_CARGO = new CbType();
-		trace("CB_CARGO is " + CB_CARGO);
-		CB_SHIP_SENSOR_RANGE = new CbType();
-		trace("CB_SHIP_SENSOR_RANGE is " + CB_SHIP_SENSOR_RANGE);
-		CB_TERRAIN = new CbType();
-		trace("CB_TERRAIN is " + CB_TERRAIN);
-		CB_TOWABLE = new CbType();
-		trace("CB_TOWABLE is " + CB_TOWABLE);
-		CB_DESTRUCTIBLE = new CbType();
-		trace("CB_DESTRUCTIBLE is " + CB_DESTRUCTIBLE);
+		CB_ITEM = new CbType();
+		trace("CB_ITEM is " + CB_ITEM);
 	}
 }
