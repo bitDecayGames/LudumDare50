f55aa67327cd374360ce6b7b8907136b1a647344
mwingfield <mwingfie@gmail.com>
2022-04-02 19:52:24 -0600

    breathing tray

--- source/entities/TrayHand.hx
+++ source/entities/TrayHand.hx
@@ -1,5 +1,6 @@
 package entities;
 
+import flixel.math.FlxPoint;
 import flixel.FlxG;
 import nape.geom.Vec2;
 import nape.phys.BodyType;
@@ -10,40 +11,66 @@ class TrayHand extends PhysicsThing {
 	var MAX_POSITION:Float = 10;
 	var MOVE_DISTANCE:Float = 1;
 	var MOVE_TIME:Float = 0.0625;
+	var OCELLATE_GRAVITY:FlxPoint = FlxPoint.get(.1, .2);
+	var OCELLATE_MAGNITUDE:Float = 10.0;
 
 	var initPosition:Float = 0;
+
+	private var ocellateCenter:FlxPoint;
 
 	public function new(x:Float = 0, y:Float = 0) {
 		super(x, y, AssetPaths.trayHand__png, AssetPaths.trayHandBody__png, 20, 5, BodyType.KINEMATIC, false, jakeTanium());
 
 		initPosition = body.position.x;
+		ocellateCenter = FlxPoint.get(body.position.x + OCELLATE_MAGNITUDE, body.position.y + OCELLATE_MAGNITUDE);
 	}
 
 	override public function update(delta:Float) {
 		super.update(delta);
 
-		var moveDistance = 0.0;
+		ocellate();
+		// var moveDistance = 0.0;
 
-		if (FlxG.keys.anyPressed([LEFT, A])) {
-			moveDistance = -MOVE_DISTANCE;
-		} else if (FlxG.keys.anyPressed([RIGHT, D])) {
-			moveDistance = MOVE_DISTANCE;
-		}
+		// if (FlxG.keys.anyPressed([LEFT, A])) {
+		// 	moveDistance = -MOVE_DISTANCE;
+		// } else if (FlxG.keys.anyPressed([RIGHT, D])) {
+		// 	moveDistance = MOVE_DISTANCE;
+		// }
 
-		if (moveDistance != 0.0) {
-			var targetPosition = new Vec2();
-			targetPosition.y = body.position.y;
-			targetPosition.x = moveDistance + body.position.x;
+		// if (moveDistance != 0.0) {
+		// 	var targetPosition = new Vec2();
+		// 	targetPosition.y = body.position.y;
+		// 	targetPosition.x = moveDistance + body.position.x;
 
-			if (targetPosition.x - initPosition > MAX_POSITION) {
-				targetPosition.x = initPosition + MAX_POSITION;
-			} else if (targetPosition.x - initPosition < -MAX_POSITION) {
-				targetPosition.x = initPosition - MAX_POSITION;
-			}
+		// 	if (targetPosition.x - initPosition > MAX_POSITION) {
+		// 		targetPosition.x = initPosition + MAX_POSITION;
+		// 	} else if (targetPosition.x - initPosition < -MAX_POSITION) {
+		// 		targetPosition.x = initPosition - MAX_POSITION;
+		// 	}
 
-			body.setVelocityFromTarget(targetPosition, 0, MOVE_TIME);
+		// 	body.setVelocityFromTarget(targetPosition, 0, MOVE_TIME);
+		// } else {
+		// 	body.velocity.set(new Vec2(0, 0));
+		// }
+	}
+
+	private function ocellate() {
+		var x = body.position.x;
+		var y = body.position.y;
+
+		var velX = body.velocity.x;
+		var velY = body.velocity.y;
+
+		if (x > ocellateCenter.x) {
+			velX -= OCELLATE_GRAVITY.x;
 		} else {
-			body.velocity.set(new Vec2(0, 0));
+			velX += OCELLATE_GRAVITY.x;
 		}
+		if (y > ocellateCenter.y) {
+			velY -= OCELLATE_GRAVITY.y;
+		} else {
+			velY += OCELLATE_GRAVITY.y;
+		}
+		body.velocity.set(Vec2.get(velX, velY));
 	}
 }
