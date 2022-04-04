9db655cff03fbeba69e57c5ea6ad716fc14a3a9d
mwingfield <mwingfie@gmail.com>
2022-04-03 21:45:52 -0600

    better achievements

--- source/helpers/Achievements.hx
+++ source/helpers/Achievements.hx
@@ -7,60 +7,60 @@ class Achievements {
 	public static var ICON_EIFLE_TOWER = 1;
 	public static var _heightAchievement:Bool = false;
 
-	public static function NewHeightAchievement():FlxObject {
+	public static function NewHeightAchievement(show:Bool = true):FlxObject {
 		// TODO: MW ANALYTICS
-		var a:FlxObject;
 		if (!_heightAchievement) {
-			a = new AchievementToast("4ft Tall", "Reached the pinnacle of bus-boymanship.", ICON_EIFLE_TOWER).show();
 			_heightAchievement = true;
-		} else {
-			a = new FlxObject();
+			var a = new AchievementToast("4ft Tall", "Reached the pinnacle of bus-boymanship.", ICON_EIFLE_TOWER);
+			if (show)
+				a.show();
+			return a;
 		}
-		return a;
+		return new FlxObject();
 	}
 
 	public static var ICON_CUP_STACK = 2;
 	public static var _itemCountAchievementCount:Int = 10;
 	public static var _itemCountAchievement:Bool = false;
 
-	public static function NewItemCountAchievement():FlxObject {
+	public static function NewItemCountAchievement(show:Bool = true):FlxObject {
 		// TODO: MW ANALYTICS
-		var a:FlxObject;
 		if (!_itemCountAchievement) {
-			a = new AchievementToast('Hold At Least ${_itemCountAchievementCount} Items', "Have you been working out?", ICON_CUP_STACK).show();
 			_itemCountAchievement = true;
-		} else {
-			a = new FlxObject();
+			var a = new AchievementToast('Hold At Least ${_itemCountAchievementCount} Items', "Have you been working out?", ICON_CUP_STACK);
+			if (show)
+				a.show();
+			return a;
 		}
-		return a;
+		return new FlxObject();
 	}
 
 	public static var ICON_TABLE = 1;
 	public static var _firstTableAchievement:Bool = false;
 
-	public static function NewFirstTableAchievement():FlxObject {
+	public static function NewFirstTableAchievement(show:Bool = true):FlxObject {
 		// TODO: MW ANALYTICS
-		var a:FlxObject;
 		if (!_firstTableAchievement) {
-			a = new AchievementToast("My First Table", "They grow up so fast.", ICON_TABLE).show();
 			_firstTableAchievement = true;
-		} else {
-			a = new FlxObject();
+			var a = new AchievementToast("My First Table", "They grow up so fast.", ICON_TABLE);
+			if (show)
+				a.show();
+			return a;
 		}
-		return a;
+		return new FlxObject();
 	}
 
 	public static var _shotGlassOnWineAchievement:Bool = false;
 
-	public static function NewShotGlassOnWineBottle():FlxObject {
+	public static function NewShotGlassOnWineBottle(show:Bool = true):FlxObject {
 		// TODO: MW ANALYTICS
-		var a:FlxObject;
 		if (!_shotGlassOnWineAchievement) {
-			a = new AchievementToast("Shot Glass on Wine Bottle", "In case you need just a tiny sip.", ICON_EIFLE_TOWER).show();
 			_shotGlassOnWineAchievement = true;
-		} else {
-			a = new FlxObject();
+			var a = new AchievementToast("Shot Glass on Wine Bottle", "In case you need just a tiny sip.", ICON_EIFLE_TOWER);
+			if (show)
+				a.show();
+			return a;
 		}
-		return a;
+		return new FlxObject();
 	}
 }
